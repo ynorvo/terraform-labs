90d5cb45ba5ea92ef66c39ed76855f734a9dcc08
AVMUser-61363658@LODSPRODMCA.onmicrosoft.com <AVMUser-61363658@LODSPRODMCA.onmicrosoft.com>
2026-04-29 09:26:07 -0700

    added locals

--- day1_operations/main.tf
+++ day1_operations/main.tf
@@ -1,4 +1,3 @@
-# Specifiy the provider and version
 terraform {
   required_providers {
     azurerm = {
@@ -8,18 +7,27 @@ terraform {
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
   resource_provider_registrations = "core"
 }
 
-# Create the very first resource
-resource "azurerm_resource_group" "contoso_rg" {
-  name     = "contoso_rg"
-  location = "UK South"
-
+locals {
+  prefix = "contoso"
+  region = "North Europe"
   tags = {
     cost_center = "contoso research"
   }
+}
+
+resource "azurerm_resource_group" "contoso_rg" {
+  name     = "${local.prefix}_rg"
+  location = local.region
+  tags     = local.tags
+}
+
+resource "azurerm_resource_group" "contoso_dev_rg" {
+  name     = "${local.prefix}_dev_rg"
+  location = local.region
+  tags     = local.tags
 }
